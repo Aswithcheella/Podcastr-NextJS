bd76af30fd6196f4a3b670f656f71a44ffe3bf74
AswithCheella <aswithkumarcheella@gmail.com>
2024-07-21 11:56:46 +0530

    project name changes to StoryPod

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -29,7 +29,7 @@ const LeftSidebar = () => {
         >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">
-            Podcastr
+            StoryPod
           </h1>
         </Link>
 
